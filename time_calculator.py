f0b1cc0fcdb6f8aedcf8111ef8350a34349f5b2b
unknown <russeljayhernandez@gmail.com>
2023-09-24 16:42:50 +0800

    Added a gitignore for the pycache folder. All test passed but the code needs refactoring

--- time_calculator.py
+++ time_calculator.py
@@ -8,6 +8,20 @@ def add_time(start, duration, day=""):
     l_duration = duration.split(":")
     added_hour = int(l_duration[0])
     added_minutes = int(l_duration[1])
+    day_value = 0
+    final_day = ""
+
+    # for the days
+    days = {1:'Sunday',
+            2:'Monday',
+            3:'Tuesday',
+            4:'Wednesday',
+            5:'Thurdsday',
+            6:'Friday',
+            7:'Saturday'}
+    
+    
+    
 
     # convert start_hour to 24 hour format
     if c_format == 'PM' and 12 > start_hour > 0:
@@ -43,14 +57,31 @@ def add_time(start, duration, day=""):
         if final_hour == 0:
             final_hour = 12
     
+    if day:
+        day = day.title()
+        key_list = list(days.keys())
+        val_list = list(days.values())
+        index = val_list.index(day)
+        day_value = key_list[index]
+        day_value = day_value + days_later
+        if day_value > 7:
+            day_value = day_value % 7
+            final_day = days[day_value]
+        else:
+            final_day = days[day_value]
+        if  days_later == 0:
+            return f"{final_hour}:{final_minutes:02d} {c_format}, {final_day}"
+        if days_later == 1:
+            return f"{final_hour}:{final_minutes:02d} {c_format}, {final_day} (next day)"
+        if days_later > 1:
+            return f"{final_hour}:{final_minutes:02d} {c_format}, {final_day} ({days_later} days later)"
+    
     if  days_later == 0:
-        return f"{final_hour:02d}:{final_minutes:02d} {c_format}"
+        return f"{final_hour}:{final_minutes:02d} {c_format}"
     if days_later == 1:
-        return f"{final_hour:02d}:{final_minutes:02d} {c_format} (next day)"
+        return f"{final_hour}:{final_minutes:02d} {c_format} (next day)"
     if days_later > 1:
-        return f"{final_hour:02d}:{final_minutes:02d} {c_format} ({days_later} days later)"
+        return f"{final_hour}:{final_minutes:02d} {c_format} ({days_later} days later)"
 
 
     return "There's an error!"
-
-# print(add_time("8:16 PM", "466:02"))
